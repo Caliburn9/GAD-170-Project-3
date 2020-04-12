619728987f5eee2761e59c9fe4bfb961561bf76c
Nabeel Aziz <nabeelabhan@gmail.com>
2020-04-12 11:53:17 +0400

    Re-commit and push
    Done because project changes missing from actual project

--- Assets/Scripts/Items/NerfGunItem.cs
+++ Assets/Scripts/Items/NerfGunItem.cs
@@ -13,31 +13,19 @@ public class NerfGunItem : InteractiveItem
     public float launchForce = 10;
     protected float fireRateCounter;
 
-    float timer;
-
     protected void Update()
     {
-        timer += Time.deltaTime;
     }
 
     public override void OnUse()
     {
         base.OnUse();
-        FireNow();    
+
+        //TODO: we need to determine if we can fire and if so, make the thing
     }
 
     public void FireNow()
     {
-
-        if (timer >= fireRate)
-        {
-            AudioSource audioSource = GetComponent<AudioSource>();
-            audioSource.Play();
-            GameObject dart = Instantiate(nerfDartPrefab, nerfDartSpawnLocation.position, Quaternion.identity);
-            Rigidbody rb = dart.GetComponent<Rigidbody>();
-            rb.AddForce(transform.forward * launchForce);
-            
-            timer = 0;
-        }
+        //TODO: this is where we would actually create the thing and get it on its way
     }
 }
