8b5e08fd8aa0f26c24304eef96ae4ab3dde3120e
Nabeel Aziz <nabeelabhan@gmail.com>
2020-04-13 12:11:36 +0400

    Revert

--- Assets/Scripts/Items/BoomBoxItem.cs
+++ Assets/Scripts/Items/BoomBoxItem.cs
@@ -13,23 +13,67 @@ public class BoomBoxItem : InteractiveItem
 {
     //TODO: you will need more data than this, like clips to play and a way to know which clip is playing
     protected AudioSource audioSource;
+    [SerializeField] AudioClip[] audios;
+    int song = -1;
+    bool isUsed = false;
 
-    protected override void Start()
-    {
+protected override void Start()
+{
         base.Start();
+        
+        //Retrieve the data from the AudioSource component and create an array for the attached clips
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = audios[0];
 
-        //TODO; prep the boom box
-    }
 
-    public void PlayClip()
-    {
-        //TODO; this is where you might want to setup and ensure the desire clip is playing on the source
-    }
+    //TODO; prep the boom box
+}
 
-    public override void OnUse()
-    {
+public void PlayClip()
+{
+        //Play a current song in the array
+        audioSource.clip = audios[song];
+        audioSource.Play();
+}
+
+private void Update()
+{
+        //If the boombox is playing and interacted with, the next audioclip attached is played
+        if (!audioSource.isPlaying && isUsed)
+        {
+            song++;
+            
+            //If the last audioclip is played, then the first clip is played again
+            if (song > audios.Length - 1)
+            {
+                song = 0;
+            }
+
+            PlayClip();
+        }
+    //TODO; this is where you might want to setup and ensure the desire clip is playing on the source
+}
+
+public override void OnUse()
+{
         base.OnUse();
+        //Return true if boombox object is used
+        isUsed = true;
 
-        //TODO; this where we need to go to next track and start and stop playing
+        //If the current audioclip is not the last audioclip, then play the next audioclip
+        if (song < audios.Length - 1)
+        {
+            song++;
+            PlayClip();
+        }
+        //If not then stop playing, and reset the song variable to the first audioclip and the isUsed variable as false
+        else
+        {
+            audioSource.Stop();
+            song = 0;
+            isUsed = false;
+        }
+
+    //TODO; this where we need to go to next track and start and stop playing
     }
 }
--- Assets/Scripts/Items/NerfGunItem.cs
+++ Assets/Scripts/Items/NerfGunItem.cs
@@ -9,23 +9,38 @@ public class NerfGunItem : InteractiveItem
 {
     public GameObject nerfDartPrefab;
     public Transform nerfDartSpawnLocation;
-    public float fireRate = 1;
+    public float fireRate = 1;    
     public float launchForce = 10;
     protected float fireRateCounter;
 
-    protected void Update()
+    float timer;
+
+protected void Update()
     {
+        //Increase the timer's value in relation to the in-game time
+        timer += Time.deltaTime;
     }
 
-    public override void OnUse()
-    {
-        base.OnUse();
-
+public override void OnUse()
+{
         //TODO: we need to determine if we can fire and if so, make the thing
-    }
+        base.OnUse();
+        FireNow();
+}
 
-    public void FireNow()
-    {
-        //TODO: this is where we would actually create the thing and get it on its way
+public void FireNow()
+{
+        //Fire a nerf dart, adding a force to it and playing a sound to indicate this
+        if (timer >= fireRate)
+        {
+            AudioSource audioSource = GetComponent<AudioSource>();
+            audioSource.Play();
+            GameObject dart = Instantiate(nerfDartPrefab, nerfDartSpawnLocation.position, Quaternion.identity);
+            Rigidbody rb = dart.GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * launchForce);
+
+            timer = 0;
+        }
+    //TODO: this is where we would actually create the thing and get it on its way
     }
 }
